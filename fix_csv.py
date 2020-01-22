72981db5823428fc741564b2f10289156492ad26
Jennifer Baughman <jbaughman@corelogic.com>
2020-01-22 12:46:58 -0600

    Fixes CSV files with assigned quotes and delimiters.

--- fix_csv.py
+++ fix_csv.py
@@ -34,20 +34,27 @@ def parse():
     parser.add_argument("--in-delimiter", type=str, help="Define input delimiter.",
                         dest="delimiter", required=False)
     parser.add_argument("--in-quote", type=str, help="Define input quote character.",
-                        dest="quote", required=False, default='"')
+                        dest="quote", required=False)
     args = parser.parse_args()
     return args
 
 
+def sniff(in_file):
+    with open(in_file, newline='') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        return dialect
+
+
 def main():
     args = parse()
-    data = []
-    delimiter = args.delimiter if args.delimiter else "|"
+    dialect = sniff(args.in_file)
+    delimiter = args.delimiter if args.delimiter else dialect.delimiter
+    quotechar = args.quote if args.quote else dialect.quotechar
     with open(args.in_file, newline='') as in_file:
-        reader = csv.reader(in_file, delimiter=delimiter, quotechar=args.quote)
-        data = [line for line in reader]
+        reader = csv.reader(in_file, delimiter=delimiter, quotechar=quotechar)
+        data = list(reader)
     with open(args.out_file, 'w', newline='') as out_file:
-        writer = csv.writer(out_file, quoting=csv.QUOTE_MINIMAL, quotechar=args.quote)
+        writer = csv.writer(out_file)
         writer.writerows(data)
 
 
